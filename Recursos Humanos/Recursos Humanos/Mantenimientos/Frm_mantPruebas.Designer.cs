2a728692c48a29c7db0e62e903b04d7b84ba221d
Angel Solares <asolaresm3@miumg.edu.gt>
2019-11-07 20:22:02 -0600

    Se agregaron mantenimientos de Perfiles, Medios y Curriculums

--- Recursos Humanos/Recursos Humanos/Mantenimientos/Frm_mantPruebas.Designer.cs
+++ Recursos Humanos/Recursos Humanos/Mantenimientos/Frm_mantPruebas.Designer.cs
@@ -36,10 +36,10 @@
             // navegador1
             // 
             this.navegador1.BackColor = System.Drawing.Color.White;
-            this.navegador1.Location = new System.Drawing.Point(0, 0);
+            this.navegador1.Location = new System.Drawing.Point(-13, -12);
             this.navegador1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.navegador1.Name = "navegador1";
-            this.navegador1.Size = new System.Drawing.Size(1648, 813);
+            this.navegador1.Size = new System.Drawing.Size(1630, 833);
             this.navegador1.TabIndex = 0;
             this.navegador1.Load += new System.EventHandler(this.Navegador1_Load);
             this.navegador1.MouseHover += new System.EventHandler(this.Navegador1_MouseHover);
